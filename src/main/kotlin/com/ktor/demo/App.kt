9561bd123bfcece1dba0e4368b222db8716b3710
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2020-08-09 20:48:40 +0500

    added ktor implementation

--- src/main/kotlin/com/ktor/demo/App.kt
+++ src/main/kotlin/com/ktor/demo/App.kt
@@ -3,13 +3,25 @@
  */
 package com.ktor.demo
 
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
 class App {
     val greeting: String
         get() {
             return "Hello world."
         }
 }
-
 fun main(args: Array<String>) {
-    println(App().greeting)
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World", ContentType.Text.Plain)
+            }
+        }
+    }.start(wait = true)
 }
